a8f963e75363703b648c80da8a467831c74fa08a
Matt Perlick <matt.perlick@gmail.com>
2020-08-25 17:26:29 -0400

    SQL queries and basic page info

--- api.py
+++ api.py
@@ -20,16 +20,6 @@ def api_add_test():
 		values
 			({pid},{timestamp},{result})
 		""")
-	# tid = db.get_db().query("select id from tests order by id desc limit 1;")[0][1]
-	# if result == 1:
-	# 	# positive test
-	# else if result == 0:
-	# 	# negetive test
-	# 	db.get_db().query(f"""
-	# 		insert into statuses
-	# 			(person_id, test_id, visit_id, timestamp, level)
-	# 		values
-	# 			({pid},{tid},Null,{timestamp}, 0)""")
 	return redirect(url_for('pages.pages_testReports'))
 
 @api.route('/delete-test', methods=['POST'])
@@ -51,12 +41,12 @@ def api_add_visit():
 	hours = request.form.get('hours')
 	minutes = request.form.get('minutes')
 	timestamp = datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1])).timestamp()
-	duration = (int(hours)*60) + int(minutes)
+	end_timestamp = timestamp + ((int(hours)*60) + int(minutes))*60
 	db.get_db().query(f"""
 		insert into visits 
-			(person_id, location_id, timestamp, duration_minutes)
+			(person_id, location_id, timestamp, end_timestamp)
 		values
-			({pid},{lid},{timestamp},{duration})
+			({pid},{lid},{timestamp},{end_timestamp})
 		""")
 	return redirect(url_for('pages.pages_locations'))
 
@@ -83,4 +73,6 @@ def api_delete_member():
 	uid = flask_login.current_user.get_id()
 	pid = request.form.get('id')
 	db.get_db().query("delete from people where id=" + str(pid) + " limit 1;")
+	db.get_db().query("delete from visits where person_id=" + str(pid))
+	db.get_db().query("delete from tests where person_id=" + str(pid))
 	return redirect(url_for('pages.pages_familyManager'))
--- pages.py
+++ pages.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, render_template
 import flask_login
 from database import Database as db
-from datetime import datetime
+from datetime import datetime, timedelta
 
 pages = Blueprint('pages', __name__)
 
@@ -11,8 +11,62 @@ def pages_home():
 	data = {}
 	data['title'] = "SCT"
 	data['menu'] = True
-	data['id'] = flask_login.current_user.get_id()
+	uid = flask_login.current_user.get_id()
 	data['username'] = flask_login.current_user.username
+	data['status'] = []
+	people = db.get_db().query(f"select id, first, last from people where user_id={uid}") #get all people for this user
+	for person in people:
+		clear = 0
+		infected = 0
+		#get most recent test from this person
+		status = db.get_db().query(f"""
+			select result from tests 
+			where person_id={person[0]} 
+			and timestamp<{datetime.now().timestamp()}
+			order by timestamp DESC limit 1""")
+		if len(status) == 0:
+			status = 0
+		else:
+			status = status[0][0]
+		#for each person in group, find all the places that person has visited in teh last 14 days
+		visits = db.get_db().query(f"""
+			select location_id, timestamp, end_timestamp from visits 
+			where person_id={person[0]} 
+				and timestamp>{(datetime.now()-timedelta(days=14)).timestamp()}""")
+		for visit in visits:
+			#for each place that person visited, find the people with overlapping visits
+			o_people = db.get_db().query(f"""
+				select person_id from visits 
+				where location_id={visit[0]}
+				and end_timestamp>{visit[1]}
+				and timestamp<{visit[2]}
+				and person_id!={person[0]}""")
+			for o_person in o_people:
+				#for each person, find the most recent clear of infected test
+				stat = db.get_db().query(f"""
+					select result from tests 
+					where person_id={o_person[0]} 
+					and timestamp<{visit[1]}
+					order by timestamp DESC limit 1""")
+				if stat[0][0]==1:
+					infected = infected + 1
+				else:
+					clear = clear + 1
+		data['status'].append({"name": f"{person[1]} {person[2]}", "status": status,"num_infected": infected, "num_other": clear, "num_visits": len(visits)})
+	locations = db.get_db().query(f"""
+		select first, last, name, timestamp, end_timestamp from people 
+		join visits 
+			on person_id = people.id
+		join locations 
+			on location_id = locations.id
+		where user_id={uid}
+		and timestamp>{(datetime.now()-timedelta(days=1)).timestamp()}
+		order by timestamp DESC""")
+	loc_list = []
+	for loc in locations:
+		loc_list.append(list(loc))
+		loc_list[-1][3] = str(datetime.fromtimestamp(loc[3]).time())[0:-3]
+	data['locations'] = loc_list
 	return render_template('pages/home.html', data=data)
 
 @pages.route('/locations')
@@ -40,6 +94,9 @@ def pages_locations():
 				timestamp = datetime.fromtimestamp(visit[7])
 				data['visits'][j].append(str(timestamp.date()))
 				data['visits'][j].append(str(timestamp.time()))
+			elif i == 8:
+				end_timestamp = datetime.fromtimestamp(visit[8])
+				data['visits'][j].append(int((end_timestamp - timestamp).total_seconds()//60))
 			else:
 				data['visits'][j].append(item)
 			i = i + 1
@@ -79,6 +136,47 @@ def pages_testReports():
 def pages_exposureHistory():
 	data = {}
 	data['title'] = "Exposure History"
+	data['status'] = []
+	uid = flask_login.current_user.get_id()
+	people = db.get_db().query(f"select id, first, last from people where user_id={uid}") #get all people for this user
+	for person in people:
+		infected = 0
+		clear = 0
+		#get most recent test from this person
+		status = db.get_db().query(f"""
+			select result from tests 
+			where person_id={person[0]} 
+			and timestamp<{datetime.now().timestamp()}
+			order by timestamp DESC limit 1""")
+		if len(status) == 0:
+			status = 0
+		else:
+			status = status[0][0]
+		#for each person in group, find all the places that person has visited in teh last 14 days
+		visits = db.get_db().query(f"""
+			select location_id, timestamp, end_timestamp from visits 
+			where person_id={person[0]} 
+				and timestamp>{(datetime.now()-timedelta(days=14)).timestamp()}""")
+		for visit in visits:
+			#for each place that person visited, find the people with overlapping visits
+			o_people = db.get_db().query(f"""
+				select person_id from visits 
+				where location_id={visit[0]}
+				and end_timestamp>{visit[1]}
+				and timestamp<{visit[2]}
+				and person_id!={person[0]}""")
+			for o_person in o_people:
+				#for each person, find the most recent clear or infected test
+				stat = db.get_db().query(f"""
+					select result from tests 
+					where person_id={o_person[0]} 
+					and timestamp<{visit[1]}
+					order by timestamp DESC limit 1""")
+				if stat[0][0]==1:
+					infected = infected + 1
+				else:
+					clear = clear + 1
+		data['status'].append({"name": f"{person[1]} {person[2]}", "status": status,"num_infected": infected, "num_other": clear, "num_visits": len(visits)})
 	return render_template('pages/exposure-history.html', data=data)
 
 @pages.route('/family-manager')
@@ -95,4 +193,12 @@ def pages_familyManager():
 def pages_communityExposures():
 	data = {}
 	data['title'] = "Community Exposures"
+	data['statuses'] = db.get_db().query(f"""
+		select * from (
+			select id, person_id, result, max(timestamp)
+				from tests 
+				group by person_id)
+		join people 
+			on person_id = people.id
+		where result=1""")
 	return render_template('pages/community-exposures.html', data=data)
