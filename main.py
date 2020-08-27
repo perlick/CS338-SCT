a9dfdee1d66a78ce83a9b55dbc3514758b135f77
Matt Perlick <matt.perlick@gmail.com>
2020-08-26 22:02:43 -0400

    final changes

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def unauthorized_handler():
 	return redirect(url_for('auth.auth_login'))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True)
+	app.run(host='127.0.0.1', port=8080, debug=True)
